db85b685b982dea922a3ca966a0aa346ba7c439b
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-15 20:22:53 +0300

    updating styles.css for sticky navbar

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
- window.onscroll = function() {myFunction()};
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+    var sticky = navbar.getBoundingClientRect().top;
 
-function myFunction() {
-  if (window.pageOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
