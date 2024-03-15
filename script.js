b7431dd1c694e8050038a211290d0a993a298949
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-15 12:45:21 +0300

    update index.html and style.css

--- script.js
+++
@@ -1,4 +0,0 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);/home/zawadi/Documents/phase-0/phase-0-css-issue-bot-9000
